3b5199af1f42f3daf3825b81f58fe6a8c1a62264
samth <samth@ZOOICOMPUTER>
2026-01-20 13:46:22 +0100

    nu met program.cs

--- Code/Mierentuin/Program.cs
+++ Code/Mierentuin/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace casus.Mierentuin
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Main Method");
+        }
+    }
+}
