f5c75010ce29befe99d41b495ddbc30818afcf30
Blobisblij <samthomsen@ziggo.nl>
2026-01-20 12:52:13 +0100

    Delete DatabaseScripts directory

--- DatabaseScripts/Voeg_dier_Toe_Create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Dieren (
-
-                         Id INT IDENTITY(1,1) PRIMARY KEY,
-
-                         Naam NVARCHAR(100) NOT NULL,
-
-                         Geluid DECIMAL(10,2) NOT NULL
-
-);
